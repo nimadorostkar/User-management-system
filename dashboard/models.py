c763745a33ae9660ae87f50ebd7f9de0f307acc0
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-21 17:23:57 +0330

    add post

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,12 +4,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+
 
     class Meta:
         ordering = ['-created_on']
