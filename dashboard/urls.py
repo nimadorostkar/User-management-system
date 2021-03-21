f4f250828503f5ac55344ca01e018b9411abda15
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-21 11:47:27 +0330

    direct to dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('company-list', views.send_company_list, name="company-list"),
     path('update-prices', views.update_values, name="update-prices"),
     path('get-financials', views.get_financials, name="update-prices"),
