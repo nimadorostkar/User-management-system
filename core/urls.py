a491b7ed868c492b9a0c056387cde0af021c9abd
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-21 13:00:02 +0330

    clean and debug all things in step one

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Portfolio, StockHolding
 
+
 # Register your models here.
 admin.site.register(Portfolio)
 admin.site.register(StockHolding)
