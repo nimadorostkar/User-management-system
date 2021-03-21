6c74119f8a0066721a8f580f78bca9366b46457b
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-21 18:04:43 +0330

    ggggg

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from .models import Profile, Notice
+from . import models
 
 
 
 @login_required
 def dashboard(request):
-  profile = Profile.objects.filter(user=request.user)
-  notices = Notice.objects.all()
+  profile = models.Profile.objects.filter(user=request.user)
+  notices = models.Notice.objects.filter(user=request.user)
   context = {'profile': profile }
   return render(request, 'dashboard/dashboard.html', context)
