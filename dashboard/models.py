b69319c9e411f4c296f7e1958249ed38c4520737
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-21 17:24:29 +0330

    debug post model

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,18 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+
+
+
 
 
 class Profile(models.Model):
