6c7dd781a50cf4e8c005760b5a941b6d607871b8
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-21 11:30:56 +0330

    again add home

--- portfolio_management_system/urls.py
+++ portfolio_management_system/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
     path('', include('dashboard.urls')),
 ]
