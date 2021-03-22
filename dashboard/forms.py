7a6ee3f8422ffe68a46f6d898b1159026b045f42
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 14:45:46 +0430

    create forms.py

---
+++ dashboard/forms.py
@@ -0,0 +1,9 @@
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('address', 'bank_name', 'cardـnumber')
