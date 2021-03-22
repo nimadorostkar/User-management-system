52ca24ef4bbf6b22644bf2b1410d9bec44944ae1
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 18:11:29 +0430

    rrrrr tttt

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,8 +6,7 @@ from django.conf import settings
 from .models import Profile, Notice
 from . import models
 from django.db import transaction
-
-
+from .forms import ProfileForm, CreateUserForm
 
 
 
@@ -19,3 +18,21 @@ def dashboard(request):
   notices = models.Notice.objects.filter(user=request.user).order_by('-created_on')
   context = {'profile': profile, 'notices':notices }
   return render(request, 'dashboard/dashboard.html', context)
+
+
+
+
+@login_required
+def updateProfile(request, pk):
+
+	profile = Profile.objects.get(id=pk)
+	form = ProfileForm(instance=profile)
+
+	if request.method == 'POST':
+		form = ProfileForm(request.POST, instance=profile)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'dashboard/dashboard.html', context)
