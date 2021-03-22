21cddc0f9bb914fa8ef6b10c1cfcc6ec3aa0a4b8
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 19:29:50 +0430

    ------

--- dashboard/views.py
+++ dashboard/views.py
@@ -28,38 +28,23 @@ def dashboard(request):
           user_form.save()
           profile_form.save()
           messages.success(request, _('Your profile was successfully updated!'))
-          return redirect('settings:profile')
+          return redirect('settings:dashboard')
       else:
           messages.error(request, _('Please correct the error below.'))
   else:
       user_form = UserForm(instance=request.user)
       profile_form = ProfileForm(instance=request.user.profile)
 
-
-  context = {'profile': profile, 'notices':notices, 'user_form': user_form, 'profile_form': profile_form }
+  context = {
+  'profile': profile,
+  'notices': notices,
+  'user_form': user_form,
+  'profile_form': profile_form }
   return render(request, 'dashboard/dashboard.html', context)
 
 
 
 
-#-------------------------------------------------
-@login_required
-@transaction.atomic
-def update_profile(request):
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, _('Your profile was successfully updated!'))
-            return redirect('settings:profile')
-        else:
-            messages.error(request, _('Please correct the error below.'))
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'profiles/profile.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
+
+
+#-------------------------- end views ------------------------
