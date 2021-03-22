277df5a4b4a9beb940aa555ad6bbcf31035af676
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 15:14:28 +0430

    debug debug

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,19 +5,19 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Profile
 
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
 
-class ProfileForm(forms.ModelForm):
+class createUserForm(UserCreationForm):
+    class meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+class profileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('address', 'bank_name', 'cardـnumber', 'account_holder')
-        #The labels attribute is optional. It is used to define the labels of the form fields created
+        fields = ['bank_name', 'identity_number', 'address']
+#The labels attribute is optional. It is used to define the labels of the form fields created
         labels = {
-                "address": _("address     "),
-                "bank_name": _("bank_name"),
-                "cardـnumber": _("cardـnumberrr"),
-                "account_holder": _("account holder"),
+                "bank_name": _("bank_name Name     "),
+                "identity_number": _("identity_number Address"),
+                "address": _("Street Address"),
                 }
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,19 @@
 import random
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from .models import Profile, Notice
 from . import models
 from django.db import transaction
+from django.contrib.auth import authenticate, login,
+from django.contrib import messages
+from .forms import createUserForm, profileForm
+
+
+
+
+
 
 
 
@@ -19,23 +27,24 @@ def dashboard(request):
 
 
 
-@login_required
-@transaction.atomic
-def update_profile(request):
+def registerPage(request):
     if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, _('Your profile was successfully updated!'))
-            return redirect('settings:profile')
-        else:
-            messages.error(request, _('Please correct the error below.'))
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'profiles/profile.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
+        form = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+
+            #we don't save the profile_form here because we have to first get the value of profile_form, assign the user to the OneToOneField created in models before we now save the profile_form.
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            profile.save()
+
+            messages.success(request,  'Your account has been successfully created')
+
+            return redirect('login')
+
+    context = {'form': form, 'profile_form': profile_form}
+    return render(request, 'app_name/register.html', context)
