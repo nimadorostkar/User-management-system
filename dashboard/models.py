c6031a8b1372a70808b4c1ba8561d30d29f94028
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 14:16:41 +0430

    extent user

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 
@@ -39,6 +41,15 @@ class Profile(models.Model):
   cardـnumber = models.CharField(max_length=16,null=True, blank=True,verbose_name = " شماره کارت بانک  ")
   account_holder = models.CharField(max_length=150,null=True, blank=True,verbose_name = " نام صاحب حساب ")
 
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+      if created:
+          Profile.objects.create(user=instance)
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
+
 
   class Meta:
       verbose_name = "پروفایل"
@@ -46,6 +57,8 @@ class Profile(models.Model):
 
   def __str__(self):
     return "پروفایل : " + str(self.user)
+
+
 
 
 
