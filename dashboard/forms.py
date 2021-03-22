0ceda6f34d9174fa5e81fdd7c1e2dac51095227f
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 15:01:49 +0430

    add more imports and labales

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from .models import Profile
 
 
 class UserForm(forms.ModelForm):
@@ -12,3 +14,10 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('address', 'bank_name', 'cardـnumber', 'account_holder')
+        #The labels attribute is optional. It is used to define the labels of the form fields created
+        labels = {
+                "address": _("address     "),
+                "bank_name": _("bank_name"),
+                "cardـnumber": _("cardـnumberrr"),
+                "account_holder": _("account holder"),
+                }
