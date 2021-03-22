27e02d8e6c14f233fb3607d91ad1a831fbca00af
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-22 17:56:37 +0430

    create views for user

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,3 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+
+
+
+
+class ProfileForm(ModelForm):
+	class Meta:
+		model = Profile
+		fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
