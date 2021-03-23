c88e7012fe3b78c9704866b4674cd1c478590f3c
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-23 12:32:26 +0430

    add request.FILES

--- dashboard/views.py
+++ dashboard/views.py
@@ -23,7 +23,7 @@ def dashboard(request):
 
   if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
