6b3cf63254aa087c2329ead7313cd5a16751d1b4
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-25 03:40:40 +0430

    add LogEntry to admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,13 @@
 from .models import Profile, Notice, Payment, Submitted_files
 from django.contrib import admin
 
+from django.contrib.admin.models import LogEntry
+admin.site.register(LogEntry)
+
+
+admin.site.site_header= "  پنل مدیریت BOM "
+admin.site.site_title= "Tavankar"
+
 
 
 admin.site.register(Notice)
