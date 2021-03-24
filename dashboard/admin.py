8b64aba8e50116a4066ea47f8edef1798d15df22
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-25 03:44:53 +0430

    add ModelAdmin class to admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,16 +1,28 @@
 from .models import Profile, Notice, Payment, Submitted_files
 from django.contrib import admin
-
 from django.contrib.admin.models import LogEntry
+
+
+admin.site.site_header= " Crop "
+admin.site.site_title= " Crop "
+
+
+
+class NoticeAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+class Submitted_filesAdmin(admin.ModelAdmin):
+	list_display = ('name','description')
+class PaymentAdmin(admin.ModelAdmin):
+	list_display = ('name','image_tag','description')
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('rate','product')
+
+
+admin.site.register(models.Notice,NoticeAdmin)
+admin.site.register(models.Submitted_files,Submitted_filesAdmin)
+admin.site.register(models.Payment,PaymentAdmin)
+admin.site.register(models.Profile,ProfileAdmin)
+
+
+
 admin.site.register(LogEntry)
-
-
-admin.site.site_header= "  پنل مدیریت BOM "
-admin.site.site_title= "Tavankar"
-
-
-
-admin.site.register(Notice)
-admin.site.register(Submitted_files)
-admin.site.register(Payment)
-admin.site.register(Profile)
