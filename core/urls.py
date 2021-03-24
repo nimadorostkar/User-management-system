e2b69517f35f636cc83fe3db8f85e6414156dc95
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-24 14:36:03 +0430

    change url to django.contrib.auth.urls

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('dashboard.urls')),
 ]
 if settings.DEBUG:
