7280066fa518ebed916e7aad56e38b3134d06c21
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-24 14:39:48 +0430

    rr

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('dashboard.urls')),
 ]
 if settings.DEBUG:
