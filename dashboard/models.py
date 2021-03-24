0f96a9f5aec65407bb9c20e6a6b00a0a7d842fd9
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-25 03:56:50 +0430

    from django.utils.html import format_html

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.utils.html import format_html
 from django.dispatch import receiver
 from django.db import models
 
